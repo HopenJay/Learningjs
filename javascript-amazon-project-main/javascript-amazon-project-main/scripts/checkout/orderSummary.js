9f3ea03c54798172d2adab5fedb56c3d94f935f0
JaynHope <jaynhope1@gmail.com>
2025-08-20 04:03:29 -0700

    Create automated tests for orderSummary.

--- javascript-amazon-project-main/javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -21,7 +21,7 @@ export function renderOrderSummary() {
       const dateString = calculateDeliveryDate(deliveryOption);
 
       cartSummaryHTML += `
-            <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+            <div class="cart-item-container js-cart--tem js-cart-item-container-${matchingProduct.id}">
               <div class="delivery-date">
                 Delivery date: ${dateString}
               </div>
@@ -37,7 +37,7 @@ export function renderOrderSummary() {
                   <div class="product-price">
                     $${formatCurrency(matchingProduct.priceCents)}
                   </div>
-                  <div class="product-quantity">
+                  <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                     <span>
                       Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
                     </span>
@@ -46,7 +46,7 @@ export function renderOrderSummary() {
                     </span>                  
                     <input type="text" class="quantity-input  js-trial js-quantity-input-${matchingProduct.id}">
                     <span class="save-quantity-link link-primary js-save-quantity-link" data-product-id="${matchingProduct.id}" tabindex = "0">Save</span>
-                    <span class="delete-quantity-link link-primary js-delete-link" data-product-id = "${matchingProduct.id}">
+                    <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id = "${matchingProduct.id}">
                       Delete
                     </span>
                   </div>
@@ -226,7 +226,7 @@ export function renderOrderSummary() {
     // for this stuff supersimpledev made use of a const cartQuantity to put the function into it. Just thought to leave mine the way it is just for maintaining the originality.
   };
 
-  updateCartQuantity();
+  // updateCartQuantity(); TODO: 19/08/2025 Don't forget to uncomment this function I commented it to test things out in jasmine
   // The code below was not needed I completely mistook 14e but I'm going to live the code as a proof of my thinking outside the box. Also remeber I also needed to add a parameter in the updateCartQuantity in delete link function
   // function updateCartQuantity(dom) {
   //     let cartQuantity = 0;
